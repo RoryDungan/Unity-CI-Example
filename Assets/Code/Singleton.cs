9058478114a93a751f7f16366ff9ae759eb95016
Rory Dungan <rorydungan@gmail.com>
2018-11-25 14:37:39 +1000

    Set up ViewModel for the GameController

--- Assets/Code/Singleton.cs
+++ Assets/Code/Singleton.cs
@@ -3,5 +3,5 @@
 public abstract class Singleton<T> where T: class, new()
 {
     private static T instance;
-    public T Instance => instance ?? (instance = new T());
+    public static T Instance => instance ?? (instance = new T());
 }
---
+++ Assets/Code/ViewModels/GameControllerViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityWeld.Binding;
+
+/// <summary>
+/// View-model for the game controller.
+/// </summary>
+public class GameControllerViewModel : MonoBehaviour, INotifyPropertyChanged
+{
+    private GameController gameController;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    [Binding]
+    public int Score => this.gameController.Score;
+
+    // Use this for initialization
+    private void Start()
+    {
+        this.gameController = GameController.Instance;
+        this.gameController.OnScoreChanged += this.GameController_OnScoreChanged;
+    }
+
+    private void OnDestroy()
+    {
+        this.gameController.OnScoreChanged -= this.GameController_OnScoreChanged;
+    }
+
+    private void GameController_OnScoreChanged(object sender, GameController.ScoreChangedEventArgs eventArgs)
+    {
+        OnPropertyChanged(nameof(Score));
+    }
+
+    private void OnPropertyChanged(string propertyName)
+    {
+        if (this.PropertyChanged != null)
+        {
+            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
