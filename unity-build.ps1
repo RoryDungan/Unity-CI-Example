01b5b5a9c0e2f3c84623dfb761c80c482156c17e
Rory Dungan <rorydungan@gmail.com>
2019-03-22 16:23:29 +1000

    Add scripts for running Unity from the command line

---
+++ unity-build.ps1
@@ -0,0 +1,47 @@
+﻿function Check-Env($variableName, $message) {
+    if (![System.Environment]::GetEnvironmentVariable($variableName)) {
+        echo $variableName environment variable not set!
+        echo $message
+        Exit 1
+    }
+}
+
+Check-Env('BUILD_TAG', 'This should be set by Jenkins and is required to determine the build output path.')
+Check-Env('BUILDS_DIR', 'This should be set to the directory to save builds in.')
+Check-Env('BUILD_TARGET', 'This should be set to the platform to build for.')
+
+# Set project source to the full path to the directory containing this script
+$src = $PSScriptRoot
+$outdir = Join-Path -Path $Env:BUILDS_DIR -ChildPath $Env:BUILD_TAG
+
+# Create temporary file for the Unity log. 
+# We need this because if multiple editor instances are open we can't rely on the contents of the default Editor.log
+# containing output just from this instance.
+$logfile = [System.IO.Path]::GetTempFileName()
+# Path to Unity - change to match your install location
+$unity = 'C:\Program Files\Unity\Hub\Editor\2018.3.7f1\Editor\Unity.exe'
+
+Write-Host "Building to $OUTDIR"
+Write-Host "Build started:" (Get-Date).ToString()
+
+$process = $null
+try {
+    $arguments = "-nographics -quit -batchmode -buildTarget $Env:BUILD_TARGET -projectPath $src -logFile '$logFile' -executeMethod Build.BuildUtility.MakeBuild -buildPath '$outdir'"
+    $process = Start-Process $unity -PassThru
+    Wait-Process -Id $process.Id
+
+    # Write log output to standard output. TODO: Can we tail the file and output in real time as it's being written to?
+    Get-Content $logfile
+}
+finally {
+    # Kill Unity if this script was killed. This makes sure the build actually stops if we cancel the job in Jenkins.
+    if ($process -ne $null) {
+        Stop-Process $process.Id
+    }
+
+    # Remove temporary log file
+    Remove-Item $logfile
+}
+
+# Exit script with Unity exit code
+Exit $process.ExitCode
---
+++ unity-build.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Kill subprocesses if this script is stoped. This makes sure Unity actually 
+# stops if you cancel the Jenkins job.
+trap 'kill $(jobs -pr)' SIGINT SIGTERM 
+
+check_env() {
+    if [ -z "${!1}" ]
+    then
+        echo "${1} environment variable not set!"
+        echo $2
+        exit 1
+    fi
+}
+
+check_env BUILD_TAG "This should be set by Jenkins and is required to determine the build output path."
+check_env BUILDS_DIR "This should be set to the directory to save builds in."
+check_env BUILD_TARGET "This should be set to the platform to build for (iOS or Android)."
+
+# Set project source to the full path to the directory containing this script
+SRC=$( cd "$(dirname $0)" ; pwd -P )
+OUTDIR=${BUILDS_DIR}/${BUILD_TAG}
+# Create a temporary file for the log file.
+# We need this because if multiple editor instances are open we can't rely on 
+# the contents of the default Editor.log containing output just from this instance.
+LOGFILE=$(mktemp)
+# Path to Unity - change to match your install location
+UNITY=/Applications/Unity/Unity.app/Contents/MacOS/Unity 
+
+echo Building to $OUTDIR
+echo Build started: `date`
+
+$UNITY -nographics -quit -batchmode -buildTarget $BUILD_TARGET -projectPath "$SRC" -logFile "$LOGFILE" -executeMethod Build.BuildUtility.MakeBuild -buildPath "$OUTDIR" &
+unitypid=$!
+
+# Pipe Unity log output to stdout
+#
+# Unity doesn't output to stdout but it does have a log file. We can pipe this to stdout
+# by starting Unity in the background (using &) and using tail to read the log file and 
+# write it to stdout. We want this script to return the same value as Unity returned so
+# we save the Unity return value in unityreturn.
+#
+# Note that we also have to start tail in the background so that we can wait for Unity
+# to exit and then kill tail. Otherwise, tail will keep running forever. 
+tail -f -n +0 "$LOGFILE" &
+tailpid=$!
+wait $unitypid # Wait for Unity to finish
+unityreturn=$?
+kill $tailpid # Kill tail now that Unity has finished writing log messages.
+wait $tailpid 2>/dev/null # Pipe stderr to /dev/null to suppress message saying process was killed.
+
+# Remove temp file
+rm "$LOGFILE"
+
+# Return with the same value as the Unity process
+exit $unityreturn
