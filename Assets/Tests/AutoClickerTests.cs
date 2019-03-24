da47c7e04caf35b59ae9ad2f0f5012f918eb11ad
Rory Dungan <rorydungan@gmail.com>
2019-03-25 00:44:00 +1000

    Write a couple of simple tests

---
+++ Assets/Tests/AutoClickerTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class AutoClickerTests
+{
+    private AutoClickerItem testObject;
+
+    private void Init()
+    {
+        testObject = new AutoClickerItem();
+    }
+
+    [Test]
+    public void returns_zero_initially()
+    {
+        Init();
+
+        var value = testObject.Update(0f);
+
+        Assert.That(value, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void returns_1_after_updating_past_time_threshold()
+    {
+        Init();
+
+        var value = testObject.Update(0.5f);
+        value += testObject.Update(1f);
+        value += testObject.Update(1.5f);
+
+        Assert.That(value, Is.EqualTo(1));
+    }
+}
